e0e72d9e5b72cfa28a5c772e98e9e7eadbaf08f2
seshnx <seshnx@gmail.com>
2026-03-18 17:31:28 -0700

    fix: commit convex/_generated stubs and unblock from .gitignore
    - Remove convex/_generated from .gitignore so stub files can be tracked
    - Add stub files: api.ts, dataModel.ts, server.ts, index.ts
      These allow the Vite build to resolve Convex imports before npx convex dev is run

---
+++ convex/_generated/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/**
+ * Generated API index.
+ * This is a STUB file generated to allow the build to succeed before `npx convex dev` is run.
+ * When you run `npx convex dev` this file will be replaced.
+ *
+ * @module
+ */
+
+// Re-export the api stub for imports like: import { api } from '../../convex/_generated'
+export { api, internal } from "./api";
+export type { Id, Doc } from "./dataModel";
